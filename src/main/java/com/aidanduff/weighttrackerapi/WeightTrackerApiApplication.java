0c9500bafa3dc089aa4ccd0dc30d514f5ede49b2
Aidan Duff <aidanpatrickduff@gmail.com>
2020-09-07 19:29:21 +0100

    Initial project creation. Web, SQL and JPA dependencies.

---
+++ src/main/java/com/aidanduff/weighttrackerapi/WeightTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.aidanduff.weighttrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aidanduff/weighttrackerapi/WeightTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidanduff.weighttrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
