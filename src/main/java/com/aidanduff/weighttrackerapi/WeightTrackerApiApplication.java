00d61d168847062a2f55adb8d4295345127dbcb7
Aidan Duff <aidanpatrickduff@gmail.com>
2020-09-16 20:46:48 +0100

    Added security classes. Disabled csrf.

--- src/main/java/com/aidanduff/weighttrackerapi/WeightTrackerApiApplication.java
+++ src/main/java/com/aidanduff/weighttrackerapi/WeightTrackerApiApplication.java
@@ -2,8 +2,12 @@ package com.aidanduff.weighttrackerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.aidanduff.weighttrackerapi.dao.UserRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class WeightTrackerApiApplication {
 
 	public static void main(String[] args) {
