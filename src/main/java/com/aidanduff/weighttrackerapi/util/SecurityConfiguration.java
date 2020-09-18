ac6a1a7296d3ba702128644d981fe81c208eb8a4
Aidan Duff <aidanpatrickduff@gmail.com>
2020-09-18 11:39:03 +0100

    UserDetails service and utility class added

--- src/main/java/com/aidanduff/weighttrackerapi/util/SecurityConfiguration.java
+++ src/main/java/com/aidanduff/weighttrackerapi/util/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.aidanduff.weighttrackerapi.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,11 +25,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-//			.antMatchers("/admin").hasRole("ADMIN")
-//			.antMatchers("/user").hasAnyRole("ADMIN", "USER")
+			.antMatchers("/admin").hasRole("ADMIN")
+			.antMatchers("/weightRecords").hasAnyRole("ADMIN", "USER")
 			.antMatchers("/").permitAll()
-			.and().formLogin()
-			.and().csrf().disable();
+			.and().formLogin();
+//			.and().csrf().disable();
 	}
 	
 	@Bean
